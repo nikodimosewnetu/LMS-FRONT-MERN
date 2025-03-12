1b1a1771911ebb1ae1bb26f29465221648a20e9e
Nikodimos-Developer <98909873+nikodimosewnetu@users.noreply.github.com>
2025-03-12 23:53:17 +0300

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://lms-back-mern-2.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
